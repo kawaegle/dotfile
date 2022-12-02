670a86ef98d2ccc982afe67db15e208d5276f964
kawaegle <alecromski@gmail.com>
2022-12-02 16:46:55 +0100

    update on nvim tokyonight theme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,12 +1,23 @@
-local colorscheme = "tokyonight"
+require("tokyonight").setup({
+  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "night", -- The theme is used when the background is set to light
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+  on_colors = function(colors) end,
+  on_highlights = function(highlights, colors) end,
+})
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-    vim.notify("Colorscheme " .. colorscheme .. " not found !")
-    return
-end
-
-vim.g.tokyonight_style = night
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebar = {"qf", "terminal", "packer"}
-vim.g.tokyonight_colors = {hint = "orange", error = "#ff0000"}
+vim.cmd[[colorscheme tokyonight]]
--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,35 +1,6 @@
--- stylua: ignore
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#569b80',
-  black  = '#24283b',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#b261ae',
-  grey   = '#3e4970'
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
-
 require('lualine').setup {
   options = {
-    theme = bubbles_theme,
+    theme = tokyonight,
     component_separators = '|',
     section_separators = { left = '', right = '' },
   },
