6f127b323982e456306fe80db1232db4c897a31d
kawaegle <alecromski@gmail.com>
2022-12-14 16:53:43 +0100

    use builtin tokyonight

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,34 +1,34 @@
-local colors = {
-    blue   = '#80A0FF',
-    cyan   = '#569B80',
-    black  = '#24283B',
-    white  = '#C6C6C6',
-    red    = '#FF5189',
-    violet = '#B261AE',
-    grey   = '#3E4970'
-}
-
-local custom_theme = {
-    normal = {
-        a = { fg = colors.black, bg = colors.violet },
-        b = { fg = colors.white, bg = colors.grey },
-        c = { fg = colors.black, bg = colors.black },
-    },
-
-    insert = { a = { fg = colors.black, bg = colors.blue } },
-    visual = { a = { fg = colors.black, bg = colors.cyan } },
-    replace = { a = { fg = colors.black, bg = colors.red } },
-
-    inactive = {
-        a = { fg = colors.white, bg = colors.black },
-        b = { fg = colors.white, bg = colors.black },
-        c = { fg = colors.black, bg = colors.black },
-    },
-}
+--[[ local colors = { ]]
+--[[     blue   = '#80A0FF', ]]
+--[[     cyan   = '#569B80', ]]
+--[[     black  = '#24283B', ]]
+--[[     white  = '#C6C6C6', ]]
+--[[     red    = '#FF5189', ]]
+--[[     violet = '#B261AE', ]]
+--[[     grey   = '#3E4970' ]]
+--[[ } ]]
+--[[]]
+--[[ local custom_theme = { ]]
+--[[     normal = { ]]
+--[[         a = { fg = colors.black, bg = colors.violet }, ]]
+--[[         b = { fg = colors.white, bg = colors.grey }, ]]
+--[[         c = { fg = colors.black, bg = colors.black }, ]]
+--[[     }, ]]
+--[[]]
+--[[     insert = { a = { fg = colors.black, bg = colors.blue } }, ]]
+--[[     visual = { a = { fg = colors.black, bg = colors.cyan } }, ]]
+--[[     replace = { a = { fg = colors.black, bg = colors.red } }, ]]
+--[[]]
+--[[     inactive = { ]]
+--[[         a = { fg = colors.white, bg = colors.black }, ]]
+--[[         b = { fg = colors.white, bg = colors.black }, ]]
+--[[         c = { fg = colors.black, bg = colors.black }, ]]
+--[[     }, ]]
+--[[ } ]]
 
 require('lualine').setup {
     options = {
-        theme = custom_theme,
+        theme = tokyonight,
         always_divide_middle = true,
         component_separators = '|',
         section_separators = {left = '', right = ''},
