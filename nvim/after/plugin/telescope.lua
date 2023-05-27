e99f614586890abeb6624e299b4ec2a62290c111
kawaegle <alecromski@gmail.com>
2023-05-27 21:28:29 +0200

    sh to grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,7 +5,7 @@ end
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>sh', function()
+vim.keymap.set('n', '<leader>gr', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
 vim.keymap.set("n", "<leader>t", ":TodoTelescope<CR>")
