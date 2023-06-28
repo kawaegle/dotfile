062b2dee3aea1e7018e50fd7b4862628acfaa34c
kawaegle <alecromski@gmail.com>
2023-06-28 18:28:46 +0200

    add nvim-tree plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -20,60 +20,59 @@ require("packer").init {
 }
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'folke/tokyonight.nvim'
-    use 'mbbill/undotree'
-    use 'romainl/vim-qf'
-    use {'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    requires = {'nvim-lua/plenary.nvim'}
-}
-use {'nvim-treesitter/nvim-treesitter',
-{run = ':TSUpdate'}
+    use 'wbthomason/packer.nvim' -- packet manager
+    use 'folke/tokyonight.nvim' -- themes
+    use 'mbbill/undotree' -- manage undo as a tree
+    use 'romainl/vim-qf' -- better quickfix management (TODO: implement it with telescope)
+    use {'nvim-telescope/telescope.nvim', -- find, preview, picker
+        tag = '0.1.1',
+        requires = 'nvim-lua/plenary.nvim'
     }
+    use {'nvim-treesitter/nvim-treesitter', -- treesitter algo
+        run = ':TSUpdate'
+    }
+    -- file tree explorer
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- icons
+        },
+    }
+    -- LSP management
     use {'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
-
-        -- Autocompletion
-        {'onsails/lspkind-nvim'},
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
-
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            'neovim/nvim-lspconfig',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            -- Autocompletion
+            'onsails/lspkind-nvim',
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            -- Snippets
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
+        }
     }
-}
-use {'Nero-F/vim-tek-header'} -- need to remove it soon as possible
-use {'folke/todo-comments.nvim',
-requires = "nvim-lua/plenary.nvim"
+    use 'Nero-F/vim-tek-header' -- need to remove it soon as possible
+    use {'folke/todo-comments.nvim', -- manage what I have ToDo
+        requires = "nvim-lua/plenary.nvim"
     }
-    use {'nvim-lua/popup.nvim'} --popup plugin needed by most of plugins
-    use {'HiPhish/nvim-ts-rainbow2'}
-    use {'windwp/nvim-autopairs'} --allow fast autopairs based on treesitter
-    use {'numToStr/Comment.nvim',
-    requires = 'JoosepAlviste/nvim-ts-context-commentstring'
-} --comment depends on the filetype
-use {'lewis6991/gitsigns.nvim'} --show git symbols on modified file
-use {'normen/vim-pio'} --support platformio on nvim
-use {'nvim-lualine/lualine.nvim', --tabline easy to configure
-requires = {'nvim-tree/nvim-web-devicons'}
+    use 'nvim-lua/popup.nvim' --popup plugin needed by most of plugins
+    use 'HiPhish/nvim-ts-rainbow2' -- treesitter parentheses coloring
+    use 'windwp/nvim-autopairs' --allow fast autopairs based on treesitter
+    use {'numToStr/Comment.nvim', --comment depends on the filetype
+        requires = 'JoosepAlviste/nvim-ts-context-commentstring'
     }
-    use {'nvim-tree/nvim-tree.lua',
-    requires = {'nvim-tree/nvim-web-devicons'},
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-}
-use {'folke/todo-comments.nvim',
-requires = "nvim-lua/plenary.nvim"
+    use 'lewis6991/gitsigns.nvim' --show git symbols on modified file
+    use 'normen/vim-pio' -- support platformio on nvim
+    use {'nvim-lualine/lualine.nvim', -- statusline easy to configure
+        requires = 'nvim-tree/nvim-web-devicons',
     }
     if packer_bootstrap then
         require('packer').sync()
