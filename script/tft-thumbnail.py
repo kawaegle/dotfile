e076e8394b4b8480d31a6d9562f07698f212535a
K4W43GL3 <kawaegle@mylittlepwnies.tech>
2024-03-07 15:29:13 +0100

    update script and remove useless

--- script/tft-thumbnail.py
+++ script/tft-thumbnail.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python
-# TFT Thumbnail for Two Trees Sapphire Pro/Plus (and MKS Robin Nano TFT35)
-# This is a Prusa Slicer post processor that generates compatible preview images.
-# It is released under the terms of the AGPLv3 or higher.
-# 2022/10/5 Initial release @whpthomas
-
-# Config: Marlin 2, 100x100, 200x200, PNG GCODE Thumbnails
-# Post-Processing Script: /path/to/python3 /path/to/script/tft-thumbnail.py
-
-import sys, os, io
+import sys
 import base64
-import regex # pip install regex
+import regex
 from os.path import exists
 from io import BytesIO
-from PIL import Image # pip install Image
-
-#src: MKS-WIFI Uploader for Prusa Slicer : https://github.com/ArtificalSUN/MKS-WIFI_PS_uploader/blob/main/MKS_WIFI_PS_upload.pyw
-#src: mks-wifi plugin : https://github.com/Jeredian/mks-wifi-plugin/blob/develop/MKSPreview.py
+from PIL import Image
 
 def rgb2tft(r, g, b):
     r = r >> 3
@@ -38,36 +27,35 @@ def generate_tft(img):
         res += '\nM10086 ;'
     return res
 
-def convertPrusaThumb2TFTThumb(inputfile, outputfile): # .......... Replace PrusaSlicer's Thumbnails with TFT's Thumbnails
+def convertPrusaThumb2TFTThumb(inputfile, outputfile):
     if not exists(inputfile):
         return
-    
-    gcode_in = open(inputfile).read() # ............................................................. load entire gcode file
-    
-    #regex for parsing datas with grouped part (matched)
+
+    gcode_in = open(inputfile).read()
+
     s_pattern = '(?<=(; thumbnail begin )([0-9]+)(x)([0-9]+) ([0-9]+)\n)(.*?)(?=; thumbnail end)'
     pattern = regex.compile(s_pattern, regex.M|regex.I|regex.S )
     gcode_out = '; Postprocessed by tft-thumbnail plugin'
 
-    for match in pattern.finditer(gcode_in): #....................................................... find both 100x100 and 200x200 thumbnails
+    for match in pattern.finditer(gcode_in): 
         th_width  = match.group(2)
         th_height = match.group(4)
         th_size   = match.group(5)
-        
+
         try:
-            th_datas  = match.group(6) # ............................................................ get image datas (base64)
-            th_datas = th_datas.replace('; ', '').replace('\n', '') # ............................... without carry returns, etc
-            stream = BytesIO( base64.b64decode(th_datas) ) # ........................................ decoding base64
-            image = Image.open(stream).convert("RGB") # ............................................. for converting into PIL image
+            th_datas  = match.group(6)
+            th_datas = th_datas.replace('; ', '').replace('\n', '')
+            stream = BytesIO( base64.b64decode(th_datas) )
+            image = Image.open(stream).convert("RGB")
             stream.close()
-            gcode_out += generate_tft(image) # ...................................................... prepend PIL image to TFT GCode            
+            gcode_out += generate_tft(image)
         except:
             pass
-    
+
     try:
-        s_pattern = '; thumbnail begin .*; thumbnail end' # ............. remove thumbnails form Prusa GCode and append to TFT GCode
+        s_pattern = '; thumbnail begin .*; thumbnail end'
         gcode_out = gcode_out + '\n' +  regex.sub( s_pattern, '', gcode_in, flags = regex.M|regex.I|regex.S )
-        fileOut = open(outputfile, "w") 
+        fileOut = open(outputfile, "w")
         fileOut.write(gcode_out)
         fileOut.close()
     except:
@@ -76,20 +64,18 @@ def convertPrusaThumb2TFTThumb(inputfile, outputfile): # .......... Replace Prus
     return
 
 def main(argv):
-
     inputfile = ''
     outputfile = ''
-    try:  
-        inputfile = sys.argv[1]
+    try:
+        inputfile = argv[1]
         try:
-            outputfile = sys.argv[2]
+            outputfile = argv[2]
         except:
-            outputfile = sys.argv[1]
+            outputfile = argv[1]
     except:
         print('./tft-thumbnail infile [outfile]')
         sys.exit()
     convertPrusaThumb2TFTThumb(inputfile, outputfile)
 
-
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main(sys.argv)
