866d7e743fb8a597076a72f54c1038e9861cdd9a
K4W43GL3 <kawaegle@mylittlepwnies.tech>
2024-04-29 14:37:22 +0200

    update ui icons

--- nvim/lua/kawaegle/lazy.lua
+++ nvim/lua/kawaegle/lazy.lua
@@ -1,24 +1,42 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        "--depth 1",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {import = "kawaegle.plugins"},
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    enabled = false,
-    notify = false,
-  },
-})
+    spec = {import = "kawaegle.plugins"},
+    ui = {
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
+        checker = {
+            enabled = true,
+            notify = false,
+        },
+        change_detection = {
+            enabled = false,
+            notify = false,
+        },
+    })
