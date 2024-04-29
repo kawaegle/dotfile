b917ece375b985f991d248908936ab8f44b5a13f
K4W43GL3 <kawaegle@mylittlepwnies.tech>
2024-04-29 14:38:05 +0200

    setup color viewer

---
+++ nvim/lua/kawaegle/plugins/colorhex.lua
@@ -0,0 +1,32 @@
+local opts = {
+    filetypes = {
+        "*",
+
+        -- Excluded filteypes.
+        "!lazy", -- Commit hashes get highlighted sometimes.
+    },
+    user_default_options = {
+        RGB = true,
+        RRGGBB = true,
+        RRGGBBAA = true,
+        AARRGGBB = false,
+
+        rgb_fn = false,
+        hsl_fn = false,
+        css = true,
+        css_fn = true,
+        tailwind = true,
+
+        sass = { enable = false, parsers = { "css" }, },
+        -- mode = "background",
+        mode = "virtualtext",
+        virtualtext = "  ",
+        always_update = true,
+    },
+}
+
+return {
+    "NvChad/nvim-colorizer.lua",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = opts,
+}
