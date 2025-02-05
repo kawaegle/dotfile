2d9a039cb9f1a8b7168970593f1c1da73c5c5a0a
K4W43GL3 <kawaegle@mail.oppaiweeb.tech>
2025-02-05 17:20:51 +0100

    install dressing as input select

--- nvim/lua/kawaegle/plugins/themes.lua
+++ nvim/lua/kawaegle/plugins/themes.lua
@@ -1,29 +1,35 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "moon",
-      light_style = "dark",
-      transparent = false,
-      terminal_colors = true,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        sidebars = "dark",
-        floats = "dark",
-      },
-      sidebars = { "qf", "help" },
-      day_brightness = 0.3,
-      hide_inactive_statusline = false,
-      dim_inactive = false,
-      lualine_bold = false,
-      on_colors = function(colors) end,
-      on_highlights = function(highlights, colors) end,
-    })
-    vim.cmd("colorscheme tokyonight")
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",
+        light_style = "dark",
+        transparent = false,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          sidebars = "dark",
+          floats = "dark",
+        },
+        sidebars = { "qf", "help" },
+        day_brightness = 0.3,
+        hide_inactive_statusline = false,
+        dim_inactive = false,
+        lualine_bold = false,
+        on_colors = function(colors) end,
+        on_highlights = function(highlights, colors) end,
+      })
+      vim.cmd("colorscheme tokyonight")
+    end
+  },
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  }
 }
